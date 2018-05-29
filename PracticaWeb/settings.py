8e00c40ee4ebae41721e0c9c4252c515af9ca1aa
jmcano94 <jmcano94@gmail.com>
2018-05-29 21:14:06 +0200

    Corregido error en settings.py

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -41,7 +41,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media_cdn")
 
-MEDIA_URL = '/media/
+MEDIA_URL = '/media/'
 
 # Application definition
 
