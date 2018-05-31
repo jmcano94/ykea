70b33424b1fd24db00d536580890d8f89e339657
jmcano94 <jmcano94@gmail.com>
2018-05-31 19:19:10 +0200

    adfas

--- ykea/urls.py
+++ ykea/urls.py
@@ -3,7 +3,7 @@ from django.contrib.auth.views import login, logout
 from rest_framework import routers
 from ykea import views
 
-listOfAddresses = ['sd2018-ykea-b2.herokuapp.com']
+listOfAddresses = ['sd2018-ykea-b2', 'sd2018-ykeab6', 'sd2018-ykeab7-2','sd2018-ykeab8', 'sd2018-b9']
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^items/$', views.items, name='items'),
