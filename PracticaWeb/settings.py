cd4ad226c2d9b089eccc14f268720b785e232797
jmcano94 <jmcano94@gmail.com>
2018-06-06 19:33:31 +0200

    Añadida base de datos otra vez para que funcione heroku

--- PracticaWeb/settings.py
+++ PracticaWeb/settings.py
@@ -107,6 +107,11 @@ WSGI_APPLICATION = 'PracticaWeb.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
 
 
 # Password validation
